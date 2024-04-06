1e5b0314939fc9a739c4100f3dccb07d41ad5823
Divy Raj Saini <143434285+Dead-Bytes@users.noreply.github.com>
2024-04-07 01:11:43 +0530

    Add files via upload

--- models.py
+++ models.py
@@ -10,6 +10,7 @@ from mtcnn.mtcnn import MTCNN
 from keras_vggface.vggface import VGGFace
 from keras_vggface.utils import preprocess_input
 from tensorflow.keras.layers import Dense, Flatten
+import os
 
 
 def extract_face(img):
@@ -25,18 +26,20 @@ def extract_face(img):
     return face_array
 
 def get_embeddings(filenames):
-    faces = [extract_face(f) for f in filenames]
-    samples = asarray(faces, 'float32')
-    samples = preprocess_input(samples, version= 2)
-    model = VGGFace(model = 'resnet50', include_top=False, input_shape=(224,224,3), pooling='avg')
-    yhat = model.predict(samples)
+    yhat = []
+    for f in filenames:
+        faces = [extract_face(f)]
+        samples = asarray(faces, 'float32')
+        samples = preprocess_input(samples, version= 2)
+        model = VGGFace(model = 'senet50', include_top=False, input_shape=(224,224,3), pooling='avg')
+        yhat.append(model.predict(samples))
     return yhat
 
 def get_embedding(filename):
     face = extract_face(filename)
     sample = asarray(face, 'float32')
     sample = preprocess_input(sample, version= 2)
-    model = VGGFace(model = 'resnet50', include_top=False, input_shape=(224,224,3), pooling='avg')
+    model = VGGFace(model = 'senet50', include_top=False, input_shape=(224,224,3), pooling='avg')
     yhat = model.predict(sample)
     return yhat
 
@@ -66,31 +69,55 @@ def train_val_generators(TRAINING_DIR, VALIDATION_DIR):
   train_generator = train_datagen.flow_from_directory(directory=TRAINING_DIR,
                                                       batch_size=20,
                                                       class_mode='binary',
-                                                      target_size=(150, 150))
+                                                      target_size=(224, 224))
   validation_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rescale = 1/255)
   validation_generator = validation_datagen.flow_from_directory(directory=VALIDATION_DIR,
                                                                 batch_size=20,
                                                                 class_mode='binary',
-                                                                target_size=(150, 150))
+                                                                target_size=(224, 224))
   return train_generator, validation_generator
 
-model = VGGFace(model = 'resnet50', include_top=False, input_shape=(224,224,3), pooling='avg')
-# model 
-head = add_layer(model)
-#mondel1 = Model(input = model.inputs, output= head)
-model1 = Model(inputs=model.input, outputs=head)
+def read_database():
+    embeddings = []
+    files = []
+    for file in os.listdir('val/'):
+        for f in file:
+            if f.endswith('.jpg'):
+                files.append(f)
+                embeddings.append(get_embedding(f))
 
-filenames = ['Data/test/divy.jpg', 'Data/test/mukund.jpg','Data/test/aman.jpg', 'Data/train/divy/10.jpg', 'Data/train/mukund/100.jpg']
-embeddings = get_embeddings(filenames)
-aman_id = embeddings[2]
-mukund_id = embeddings[1]
-divy_id = embeddings[0]
+    return files, embeddings
 
-is_match(embeddings[0], embeddings[3])
 
-embeddings.shape
+def train_model(model, training_dir, validation_dir):
+    train_generator , validation_generator = train_val_generators(training_dir, validation_dir)
+    model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+    history = model.fit(train_generator, epochs = 10, validation_data = validation_generator)
+    return model
 
-model.summary()
+def matcher(filepath, train = False):
+    training_dir = 'data/train/'
+    validation_dir = 'data/val/'
 
-training_dir = 'Data/train/'
-validation_dir = 'Data/test/'
+    model = tf.keras.applications.ResNet50V2(
+                        include_top=True,
+                        weights='imagenet',
+                        classifier_activation='softmax'
+                    )
+    head = add_layer(model)
+    model1 = Model(inputs=model.input, outputs=model.output)
+    if train:
+        model = train_model(model, training_dir, validation_dir)
+
+    face = extract_face(filepath)
+    face_emb = get_embedding(face)
+    file_list , emb_list = read_database()
+    for i in range(len(emb_list)):
+        flag = is_match(face_emb, emb_list[i])
+        if flag:
+            return file_list[i]
+    return 'No Match Found'
+
+if __name__ == '__main__':
+    print("initiating_database")
+    matcher('val/ben_afflek/httpabsolumentgratuitfreefrimagesbenaffleckjpg.jpg')
